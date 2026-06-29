533fdea47b7552b0e7d700daa4a5703cddcdcc53
Onayaa <onayamindi@gmail.com>
2026-06-29 22:16:13 +0530

    Add files via upload

--- RabbitProblem.java
+++ RabbitProblem.java
@@ -3,10 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import java.util.Scanner;
+
 /**
  *
  * @author FMT
  */
 public class RabbitProblem {
     
+    public static int rabbits(int month) {
+
+        if (month == 1 || month == 2)
+            return 1;
+
+        return rabbits(month - 1) + rabbits(month - 2);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of months: ");
+        int month = input.nextInt();
+
+        System.out.println("Rabbit pairs after " + month +
+                " months = " + rabbits(month));
+    }
 }
+
