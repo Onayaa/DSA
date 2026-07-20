f8ff794f925e3efb4f01dd0d4e9c93e4692cd710
Onayaa <onayamindi@gmail.com>
2026-07-20 20:09:50 +0530

    Add files via upload

---
+++ OptimizedBubbleSortAlgorithm.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FMT
+ */
+public class OptimizedBubbleSortAlgorithm {
+    
+    public static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+    }
+}
+
+
