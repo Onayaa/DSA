4d7487c8a3b07c574213780f643475c6d5463bd9
Onayaa <onayamindi@gmail.com>
2026-07-26 22:26:35 +0530

    Add files via upload

---
+++ OptimizedInsertionSortAlgorithm.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FMT
+ */
+public class OptimizedInsertionSortAlgorithm {
+    
+    public static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+
+            if (arr[i - 1] <= key) {
+                continue;
+            }
+
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {12, 11, 13, 5, 6};
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+    }
+}
+    
+
